934982ac0c0a30568dc2c0ff08c01c032f226dbe
sula00000 <sulaimanh0052@gmail.com>
2025-05-14 19:51:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,15 +3,22 @@ import busio
 from digitalio import DigitalInOut
 from adafruit_pn532.i2c import PN532_I2C
 
+# I2C setup
 i2c = busio.I2C(board.SCL, board.SDA)
 pn532 = PN532_I2C(i2c, debug=False)
 
-pn532.SAM_configuration()
+pn532.SAM_configuration()  # Initialiser chippen
 
-print("Hold et NFC-kort tæt på læseren...")
+print("Klar. Hold kortet hen...")
 
 while True:
     uid = pn532.read_passive_target(timeout=0.5)
     if uid is not None:
-        print("UID:", [hex(i) for i in uid])
+        if len(uid) == 4:
+            uid_decimal = 0
+            for b in uid:
+                uid_decimal = (uid_decimal << 8) | b
+            print(uid_decimal)
+        else:
+            print("UID ikke 4 bytes – ignorerer.")
         break
