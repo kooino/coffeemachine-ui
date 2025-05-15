756a4cbf29711664eb48265dec96f93e77bae7ee
sula00000 <sulaimanh0052@gmail.com>
2025-05-15 18:45:19 +0200

    Delete backend/service.cpp

--- backend/service.cpp
+++
@@ -1,8 +0,0 @@
-#include "service.h"
-#include <fstream>
-
-std::string rydBestillinger() {
-    std::ofstream fil("bestillinger.txt", std::ios::trunc);
-    fil.close();
-    return "{\"status\":\"Bestillinger ryddet\"}";
-}
